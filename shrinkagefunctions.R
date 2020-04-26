d7ddfaa7ca6413f29fb9781f0aadbeeaad371c1f
V-Bernal <victor.arturo.bernal@gmail.com>
2020-04-26 20:46:13 +0200

    Improved plots, the functins do not print verbose

--- shrinkagefunctions.R
+++ shrinkagefunctions.R
@@ -19,7 +19,7 @@
 # lambda= shrinkage value 
 
 #************************************************************************
-#  * Revision History  : none
+#  * Revision History  : 30 JAN 2018 replace sd by se=sd/sqrt(sample), and (minor edits e.g. font size)
 #  **********************************************************************
 #  * Details
 #  The shrunk probability density is presented here  
@@ -29,8 +29,8 @@
 #  is performed with GeneNet [Schäfer,J. and Strimmer,K. (2005a),  GeneNet 1.2.13. CRAN.]
 #************************************************************************;
 # Refererences
-# [1] 
-# [2] Martinez, Wendy L., and Angel R. Martinez. Computational statistics handbook with MATLAB. Chapman and Hall/CRC, 2007
+# [1] Schäfer,J. and Strimmer,K. A Shrinkage Approach to Large-Scale Covariance Matrix Estimation and Implications for Functional Genomics. Stat. Appl. Genet. Mol. Biol.(2005a), 4, 1175-1189.
+# [2] Martinez, Wendy L., and Angel R. Martinez. Computational statistics handbook with MATLAB. Chapman and Hall/CRC (2007)
 # [3] Fisher, Ronald Aylmer. "The distribution of the partial correlation coefficient." Metron 3 (1924): 329-332.
 #**************************************************
 # 1. p values from the SHRUNK density
@@ -39,49 +39,88 @@
 # Use this to estimate the degrees of freedom k
 # for the shrunk probability density 
 # and compute p values
+k.shrunk <- function( p , n,lambda){
+  ## Start by simulating null hypothetic data
+  sim.pcor.S <- ggm.simulate.pcor(p, 0) # simulate identity GGM
+  sim.data.S <- ggm.simulate.data( n, sim.pcor.S) # data from identity GGM (sim.pcor.S)
+  GGM.S <- pcor.shrink(sim.data.S,lambda , verbose =FALSE)  ## infer GGM structure from sim.data.S
+  r.S <- sm2vec(GGM.S) # vectorize the GGM (symmetric matrix)
+  
+  # compute the negative log likelihood
+  nlogL.shrunk <- function(k) {
+    
+    #density.shrunk <- function(r.S) {(  ((1)^2-(r.S/(1-lambda))^2) ^((k-3)*0.5)  )/( beta(0.5, 0.5*(k-1))*(1-lambda) )}
+    log.density.shrunk <- function(x) {  ((k-3)*0.5)*log(1-(x/(1-lambda))^2) - log( beta(0.5, 0.5*(k-1)))-log(1-lambda) }
+    log.f<-log.density.shrunk(r.S)
+    
+    return(-sum(log.f))
+  }
+  
+  # minimize the negative log likelihood to find the degrees of freedom (k.fit.shrunk)
+  k.fit.shrunk <-  mle(nlogL.shrunk, start = list(k = 100), method = "L-BFGS-B", lower = c(5),
+                      upper = c(Inf))
+  #k.fit.shrunk <-  mle(minuslogl = nlogL.shrunk, start = list(k = 100),method = "BFGS")
+  #k.fit.shrunk <-  mle(nlogL.shrunk, start = list(k = 100), method = "Nelder-Mead")
+  return(k.fit.shrunk@coef[1])}
 
-      p.shrunk <- function( r, p,n ,lambda){
-      
-      # library("GeneNet")
-      # library(stats4)
-   
-        ## Start by simulating null hypothetic data
-        sim.pcor.S<-ggm.simulate.pcor(p, 0) # simulate identity GGM
-        sim.data.S<- ggm.simulate.data( n, sim.pcor.S) # data from identity GGM (sim.pcor.S)
-        GGM.S <- pcor.shrink(sim.data.S,lambda , verbose=TRUE)  ## infer GGM structure from sim.data.S
-        r.S<-sm2vec(GGM.S) # vectorize the GGM (symmetric matrix)
-       
-        # compute the negative log likelihood
-        nlogL.shrunk <- function(k) {
-              density.shrunk <- function(r.S) {(  ((1)^2-(r.S/(1-lambda))^2) ^((k-3)*0.5)  )/( beta(0.5, 0.5*(k-1))*(1-lambda) )}
-              f<-density.shrunk(r.S)
-              return(-sum(log(f)))
-        }
+p.shrunk <- function( r, p, n ,lambda){
         
-        # minimize the negative log likelihood to find the degrees of freedom (k.fit.shrunk)
-      k.fit.shrunk <-  mle(nlogL.shrunk, start = list(k = 100), method = "L-BFGS-B", lower = c(20),
-                           upper = c(Inf))
+        
+        k<-mean(unlist(lapply(c(1:25), function(w){ k.shrunk( p , n, lambda) })))
+        print(k)
+      #   ## Start by simulating null hypothetic data
+      #   sim.pcor.S <- ggm.simulate.pcor(p, 0) # simulate identity GGM
+      #   sim.data.S <- ggm.simulate.data( n, sim.pcor.S) # data from identity GGM (sim.pcor.S)
+      #   GGM.S <- pcor.shrink(sim.data.S,lambda , verbose =FALSE)  ## infer GGM structure from sim.data.S
+      #   r.S <- sm2vec(GGM.S) # vectorize the GGM (symmetric matrix)
+      #  
+      #   # compute the negative log likelihood
+      #   nlogL.shrunk <- function(k) {
+      #         density.shrunk <- function(r.S) {(  ((1)^2-(r.S/(1-lambda))^2) ^((k-3)*0.5)  )/( beta(0.5, 0.5*(k-1))*(1-lambda) )}
+      #         f<-density.shrunk(r.S)
+      #         return(-sum(log(f)))
+      #   }
+      #   
+      #   # minimize the negative log likelihood to find the degrees of freedom (k.fit.shrunk)
+      # k.fit.shrunk <-  mle(nlogL.shrunk, start = list(k = 100), method = "L-BFGS-B", lower = c(20),
+      #                      upper = c(Inf))
       
       #summary(k.fit.shrunk)
      
       ## Compute p values with k.fit.shrunk
-      density.shrunk <- function(r) {(  ((1)^2-(r/(1-lambda))^2) ^((k.fit.shrunk@coef[1]-3)*0.5)  )/( beta(0.5, 0.5*(k.fit.shrunk@coef[1]-1))*(1-lambda)) }
-
-      pval.shrunk<-matrix(Inf, length(r),1)
-      for (i in 1:length(r)){
-        int<-  integrate(density.shrunk , lower = -(1-lambda), upper = -abs( r[i] )   )
-        pval.shrunk[i]<- 2*(int$value) 
-      }
+        
+      if(lambda!=0){  
+            density.shrunk <- function(r) {(  ((1)^2-(r/(1-lambda))^2) ^((k-3)*0.5)  )/( beta(0.5, 0.5*(k-1))*(1-lambda)) }
       
-      
+            pval.shrunk<-matrix(Inf, length(r),1)
+            
+            for (i in 1:length(r)){
+              int<-  integrate(density.shrunk , lower = -(1-lambda), upper = -abs( r[i] )   )
+              pval.shrunk[i]<- 2*(int$value) 
+            }
+      }else {
+            density <- function(r) {(  ((1)^2-(r)^2) ^((k-3)*0.5)  )/( beta(0.5, 0.5*(k-1))) }
+            
+            pval.shrunk<-matrix(Inf, length(r),1)
+            
+            for (i in 1:length(r)){
+              int<-  integrate(density , lower = -(1), upper = -abs( r[i] )   )
+              pval.shrunk[i]<- 2*(int$value) 
+            }
+            
+     }
+        
+    
     return(pval.shrunk)
 }
 
+
+      
 ################################
 ## MONTECARLO p-values
 
       
-  p.montecarlo <- function( r, number,p,n ,lambda){
+  p.montecarlo <- function( r, number, p, n ,lambda){
 
         # Initialize variables
     r.monte<-matrix(NA, length(r),1) 
@@ -91,9 +130,9 @@
     # Simulate null hypothetic GGM coefficients for "number" times
       for (i in 1:number){
         
-        r.data<-ggm.simulate.data(n,diag(p))
-        r.monte.GGM<-ggm.estimate.pcor(r.data,lambda=lambda )
-        r.monte<-sm2vec(r.monte.GGM)
+        r.data <- ggm.simulate.data(n,diag(p))
+        r.monte.GGM <- ggm.estimate.pcor(r.data, lambda=lambda , verbose = FALSE )
+        r.monte <- sm2vec(r.monte.GGM)
         
         # compare the real coefficients against r.monte  
         pv<-sapply(r, function(x) sum( abs(r.monte) >=abs(x) )/length(r.monte))  
